21b7fd8eeb75b6a9caa3ab9d5055f69b8a253d12
sherif-nassif <sherif.nassif@gmail.com>
2026-04-16 18:47:46 +0000

    my initial commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project = "project-d318db8d-3948-41a2-88d" 
+  region  = "europe-north1"
+}
+
+resource "google_compute_instance" "lowest_spec_instance" {
+  name         = "stockholm-micro-instance"
+  machine_type = "e2-micro"      # The lowest spec available
+  zone         = "europe-north1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11" # Standard lightweight Linux image
+      size  = 10 # Minimum recommended size in GB
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    # Access config block allows an External IP (Public IP)
+    # Remove this block if you only want a Private IP
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  # Best practice for low-spec VMs: allow them to be stopped for maintenance
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+}
