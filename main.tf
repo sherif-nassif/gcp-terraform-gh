17b901255cf7788390b0a74cd8a5de8025523af8
sherif-nassif <sherif.nassif@gmail.com>
2026-04-17 10:13:33 +0000

    adding inbound firewall

--- main.tf
+++ main.tf
@@ -44,3 +44,16 @@ resource "google_compute_instance" "lowest_spec_instance" {
 
   tags = ["http-server"]
 }
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
