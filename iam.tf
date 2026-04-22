b5014249e24dafc5a1651b6767edc18a08a0bc6f
sherif-nassif <sherif.nassif@gmail.com>
2026-04-22 19:49:10 +0000

    correction2

--- iam.tf
+++ iam.tf
@@ -2,4 +2,8 @@ resource "google_service_account" "github_wif" {
   account_id   = "github-wif"
   display_name = "GitHub WIF Service Account"
   project      = "project-d318db8d-3948-41a2-88d"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
